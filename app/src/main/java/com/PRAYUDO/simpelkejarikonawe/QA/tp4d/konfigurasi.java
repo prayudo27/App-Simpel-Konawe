d9fda74599b756b4cd649de3430f0be08d62ddf6
prayudo <prayudo27@gmail.com>
2019-07-04 00:29:43 +0700

    first commit

---
+++ app/src/main/java/com/PRAYUDO/simpelkejarikonawe/QA/tp4d/konfigurasi.java
@@ -0,0 +1,28 @@
+package com.PRAYUDO.simpelkejarikonawe.QA.tp4d;
+
+public class konfigurasi {
+    public static final String URL_ADD="http://192.168.43.190/app_simpel_konawe/form/tp4d/create.php";
+    public static final String URL_READ_DATA_ALL="http://192.168.43.190/app_simpel_konawe/form/tp4d/read_data_all.php";
+    public static final String URL_READ_DATA="http://192.168.43.190/app_simpel_konawe/form/tp4d/read_data.php?id=";
+
+    public static final String KEY_ID_TP4D= "id";
+    public static final String KEY_NAMA_INSTANSI = "nama_instansi";
+    public static final String KEY_NAMA_KEPALA_INSTANSI = "nama_kepala_instansi";
+    public static final String KEY_ALAMAT = "alamat";
+    public static final String KEY_TELEPON = "telepon";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_KEGIATAN_DIMINTA_DIDAMPINGI = "kegiatan_diminta_didampingi";
+    public static final String KEY_NILAI_KEGIATAN = "nilai_kegiatan";
+
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID_TP4D="id";
+    public static final String TAG_NAMA_INSTANSI = "nama_instansi";
+    public static final String TAG_NAMA_KEPALA_INSTANSI = "nama_kepala_instansi";
+    public static final String TAG_ALAMAT="alamat";
+    public static final String TAG_TELEPON="telepon";
+    public static final String TAG_EMAIL="email";
+    public static final String TAG_KEGIATAN_DIMINTA_DIDAMPINGI = "kegiatan_diminta_didampingi";
+    public static final String TAG_NILAI_KEGIATAN = "nilai_kegiatan";
+
+    public static final String ID_TP4D = "id";
+}
